41279df5a8097248c8496d503610c2bbba9071bd
Katie Foster <fosterkatiea@gmail.com>
2017-05-21 21:49:41 -0600

    added a few styles and ui for addresses

--- src/js/containers/AddressContainer.jsx
+++ src/js/containers/AddressContainer.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 class AddressContainer extends React.Component {
   render() {
     return (
-      <div>This the address container</div>
+      <div className='o-flex-container o-content-container'>
+        <div className='c-box'>
+          <h3>Addresses</h3>
+          <div className='u-huey-underline'>
+            Huey:
+          </div>
+          <div className='u-duey-underline'>
+            Duey:
+          </div>
+          <div className='u-louie-underline'>
+            Louie:
+          </div>
+        </div>
+      </div>
     )
   }
 }
